1d87988fb82a8385e5732ae9828fabe208876920
PaulHaddou <paul2405.63@hotmail.fr>
2018-11-19 15:32:20 +0100

    Add the best scores, the locale storage, the song for the end

--- scripts/app.js
+++ scripts/app.js
@@ -9,10 +9,20 @@ shooter.$targets = shooter.$container.querySelector('.targets')
 shooter.$timer = shooter.$container.querySelector('.timer')
 shooter.$score = shooter.$container.querySelector('.score .value')
 shooter.score = 0
+shooter.secondsLeft = 0
+
+shooter.$bestScore = shooter.$container.querySelector('.best-score .value')
+shooter.bestScore = window.localStorage.getItem('bestScore')
+if(shooter.bestScore === null)
+{
+    shooter.bestScore = 0
+}
+shooter.bestScore = parseInt(shooter.bestScore)
+shooter.$bestScore.textContent = shooter.bestScore
 
 shooter.sounds = {}
-shooter.sounds.ding = new Audio('src/ding.mp3')
-shooter.sounds.finish = new Audio('src/finish.mp3')
+shooter.sounds.ding = new Audio('ding.mp3')
+shooter.sounds.finish = new Audio('finish.mp3')
 
 shooter.$start.addEventListener('click', () =>
 {
@@ -24,7 +34,54 @@ shooter.$start.addEventListener('click', () =>
  */
 shooter.start = () =>
 {
-    shooter.$container.classList.replace('step-start', 'step-game')
+    shooter.$container.classList.remove('step-start')
+    shooter.$container.classList.remove('step-end')
+    shooter.$container.classList.add('step-game')
+
+    shooter.secondsLeft = 4
+
+    shooter.score = 0
+    shooter.$score.textContent = shooter.score
+
+    shooter.tick()
+}
+
+shooter.end = () =>
+{
+    shooter.$container.classList.remove('step-game')
+    shooter.$container.classList.add('step-end')
+
+    shooter.sounds.finish.play()
+
+    if(shooter.score > shooter.bestScore)
+    {
+        shooter.bestScore = shooter.score
+        window.localStorage.setItem('bestScore', shooter.bestScore)
+        shooter.$bestScore.textContent = shooter.bestScore
+    }
+}
+
+shooter.tick = () =>
+{
+    shooter.secondsLeft--
+
+    if(shooter.secondsLeft === 0)
+    {
+        shooter.end()
+    }
+    else
+    {
+        if(shooter.secondsLeft > 9)
+        {
+            shooter.$timer.textContent = `00:${shooter.secondsLeft}`
+        }
+        else
+        {
+            shooter.$timer.textContent = `00:0${shooter.secondsLeft}`
+        }
+
+        window.setTimeout(shooter.tick, 1000)
+    }
 }
 
 shooter.addTarget = () =>
